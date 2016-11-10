7528e84d959232e87eba7a046b8af3277c58caac
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2016-11-10 18:01:15 -0500

    Added the directives folder

---
+++ public/users/directives/email-validator.client.directive.js
@@ -0,0 +1,45 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+angular.module('users')
+       .directive('emailValidator', emailValidator);
+
+function emailValidator() {
+    const EMAIL_REGEX = /.+\@umbc+\.edu/;
+
+    let directive = {
+        require: 'ngModel',
+        restrict: 'A',
+        transclude: true,
+        link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs, ctrl) {
+        ctrl.$validators.email = function(value) {
+            if (EMAIL_REGEX.test(value)) {
+                element.removeClass('invalid-input');
+                element.addClass('valid-input');
+
+                /*  ** TODO: Implement UMBC directory request to validate e-mail
+                scope.validateEmail(value)
+                     .success(function(data, status, headers, config) {
+                        element.removeClass('invalid-input');
+                        element.addClass('valid-input');
+                        
+                     })
+                     .error(function(data, status, headers, config) {
+
+                     });
+                 */
+            } else if (ctrl.$isEmpty(value)) {
+                element.removeClass('valid-input');
+                element.removeClass('invalid-input');
+            } else {
+                element.removeClass('valid-input');
+                element.addClass('invalid-input');
+            }
+        }
+    }
+}
