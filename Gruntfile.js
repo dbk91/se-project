d85a0b778a5f031674ede74dae8a8562e93b3238
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2016-12-01 16:18:00 -0500

    Updated dependecies for unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,62 @@ module.exports = function(grunt) {
         // Configure the grunt-contrib-watch task
         watch: {
             options: {
-                    livereload: true
+                livereload: true
             },
             js: {
                 files: ['server.js', 'config/**/*.js', 'app/**/*.js', 'public/js/*.js', 'public/modules/**/*.js']
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'index.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    watch: ['index.js', 'config/**/*.js', 'app/**/*.js'],
+                    ignore: ['node_modules/**']
+                }
+            }
+        },
+        // Configure the grunt-contrib-jshint task
+        jshint: {
+            all: {
+                src: ['server.js', 'config/**/*.js', 'app/**/*.js', 'public/js/*.js', 'public/modules/**/*.js'],
+                options: {
+                    node: true,
+                    predef: [
+                        "define",
+                        "require",
+                        "exports",
+                        "module",
+                        "describe",
+                        "before",
+                        "beforeEach",
+                        "after",
+                        "afterEach",
+                        "it",
+                        "inject",
+                        "expect"
+                    ]
+                }
+            }
+        },
+        concurrent: {
+            dev: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
             }
         }
     });
 
     // Load the external tasks
+    grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
 
     // Create the 'default' Grunt task
     grunt.registerTask('default');
---
+++ config/env/test.js
@@ -0,0 +1,8 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Set the development environment settings
+module.exports = {
+    db: 'mongodb://localhost/textforsale-test',
+    sessionSecret: 'developmentSessionSecret'
+}
